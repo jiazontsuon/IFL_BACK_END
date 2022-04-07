c984fd59efe4f49f79e1fc86fb72f098ccaf5dec
Yu <yu@hashwave.io>
2022-04-07 13:57:42 +0100

    second commit

--- src/main/java/com/example/demo/controller/IFLController.java
+++ src/main/java/com/example/demo/controller/IFLController.java
@@ -15,12 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
-    @GetMapping("/hello")
-    public Result index() {
-        Result u = Result.buildResult(Status.ERROR);
-        return u;
-    }   
+public class IFLController {
     @GetMapping("/model")
     public Model getModel(){
         Model m = new Model(Model.universalList,Model.relationMappings);
@@ -29,13 +24,9 @@ public class HelloWorldController {
     @PostMapping("/evaluate")
     public Result<String> evaluate(@RequestBody Model_Param mp){
         try {
-            // System.out.println(expr);
-            // return Result.buildResult(Status.OK);
-            System.out.println(mp.toString());
             return mp.eval();
         }
         catch(Exception e){
-            // e.printStackTrace();
             System.out.println("Error");
 			return Result.buildResult(Status.ERROR);
         }
